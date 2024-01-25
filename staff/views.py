0126ace586e18286292b0579c1f70b6342931735
M.B.A. Fadeyi <fadeyibajide@gmail.com>
2024-01-25 19:25:23 +0000

    Rest of Staff Views

--- staff/views.py
+++ staff/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 
 from cart.models import Order, Product
 
+from .forms import ProductForm
 from .mixins import StaffUserMixin
 
 
@@ -19,6 +20,31 @@ class ProductListView(LoginRequiredMixin, StaffUserMixin, generic.ListView):
     queryset = Product.objects.all()
     paginate_by = 20
     context_object_name = "products"
+
+
+class ProductCreateView(LoginRequiredMixin, StaffUserMixin, generic.CreateView):
+    template_name = "staff/product_create.html"
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse("staff:product-list")
+
+    def form_valid(self, form):
+        form.save()
+        return super(ProductCreateView, self).form_valid(form)
+
+
+class ProductUpdateView(LoginRequiredMixin, StaffUserMixin, generic.UpdateView):
+    template_name = "staff/product_update.html"
+    form_class = ProductForm
+    queryset = Product.objects.all()
+
+    def get_success_url(self):
+        return reverse("staff:product-list")
+
+    def form_valid(self, form):
+        form.save()
+        return super(ProductUpdateView, self).form_valid(form)
 
 
 class ProductDeleteView(LoginRequiredMixin, StaffUserMixin, generic.DeleteView):
